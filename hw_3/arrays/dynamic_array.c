d3a3dcdee3c622b2dd254654261573e494720097
Anusha Kamat <38510945+AnushaKamat@users.noreply.github.com>
2019-02-03 01:50:28 -0800

    Uploading v1 Q8 not working yet

---
+++ hw_3/arrays/dynamic_array.c
@@ -0,0 +1,454 @@
+#include "dynamic_array.h"
+#include "arbitrary_array.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+/* private functions *********************************************************/
+
+/* Position in the buffer of the array element at position index */
+static int index_to_offset ( const DynamicArray * da, int index ) {
+    return index + da->origin;
+}
+
+/* Position of the element at buffer position 'offset' */
+static int offset_to_index ( const DynamicArray * da, int offset ) {
+    return offset - da->origin;
+}
+
+/* Non-zero if and only if offset lies ouside the buffer */
+static int out_of_buffer ( DynamicArray * da, int offset ) {
+    return offset < 0 || offset >= da->capacity;
+}
+
+/* Makes a new buffer that is twice the size of the old buffer,
+   copies the old information into the new buffer, and deletes
+   the old buffer */
+static void extend_buffer ( DynamicArray * da ) {
+
+    double * temp = (double *) calloc ( 2 * da->capacity, sizeof(double) );
+    int new_origin = da->capacity - (da->end - da->origin)/2,
+           new_end = new_origin + (da->end - da->origin);
+
+    for ( int i=0; i<DynamicArray_size(da); i++ ) {
+        temp[new_origin+i] = DynamicArray_get(da,i);
+    }
+
+    free(da->buffer);
+    da->buffer = temp;
+
+    da->capacity = 2 * da->capacity;
+    da->origin = new_origin;
+    da->end = new_end;
+
+    return;
+
+}
+
+static DynamicArray* DynamicArray_sort (const DynamicArray * da){
+    assert(da->buffer != NULL);
+    assert(DynamicArray_size(da) > 0);
+    int sz = DynamicArray_size(da);
+    double tmp;
+    DynamicArray * sorted_da =  DynamicArray_copy (da);
+    for(int i = 0; i<sz; i++){
+        for(int j = i+1;j<sz; j++){
+            if(DynamicArray_get(sorted_da,i)>DynamicArray_get(sorted_da,j)){
+                tmp = DynamicArray_get(sorted_da,i);
+                DynamicArray_set(sorted_da,i, DynamicArray_get(sorted_da,j));
+                DynamicArray_set(sorted_da,j,tmp);
+            }
+        }
+    }
+    return sorted_da;
+}
+
+/* public functions **********************************************************/
+
+static ArbitraryArray* ptrtoDA = ArbitraryArray_new(sizeof(DynamicArray *)); 
+
+
+
+
+DynamicArray * DynamicArray_new(void) {
+    DynamicArray * da = (DynamicArray *) malloc(sizeof(DynamicArray));
+    da->capacity = DYNAMIC_ARRAY_INITIAL_CAPACITY;    
+    da->buffer = (double *) calloc ( da->capacity, sizeof(double) ); 
+    da->origin = da->capacity / 2;
+    da->end = da->origin;
+    ArbitraryArray_push(ptrtoDA,da);
+    return da;
+}
+
+void DynamicArray_destroy(DynamicArray * da) {
+    
+    free(da->buffer);
+    da->buffer = NULL;
+    ArbitraryArray_remove_ptr(ptrtoDA, da);
+    return;
+}
+
+int DynamicArray_size(const DynamicArray * da) {
+    assert(da->buffer != NULL);
+    return da->end - da->origin;
+}
+
+char * DynamicArray_to_string(const DynamicArray * da) {
+    assert(da->buffer != NULL);
+    char * str = (char *) calloc (20,DynamicArray_size(da)),
+         temp[20];
+    int j = 1;
+    str[0] = '[';
+    for ( int i=0; i < DynamicArray_size(da); i++ ) {
+        if ( DynamicArray_get(da,i) == 0 ) {
+            snprintf ( temp, 20, "0" );
+        } else {
+            snprintf ( temp, 20, "%.5lf", DynamicArray_get(da,i) ); 
+        }
+        if ( i < DynamicArray_size(da) - 1 ) {
+            sprintf( str + j, "%s,", temp);
+            j += strlen(temp) + 1;
+        } else {
+            sprintf( str + j, "%s", temp);
+            j += strlen(temp);
+        }
+    }
+    str[j] = ']';
+    return str;
+}
+
+void DynamicArray_print_debug_info(const DynamicArray * da) {
+
+    char * s = DynamicArray_to_string(da);
+    printf ( "  %s\n", s);
+    printf ( "  capacity: %d\n  origin: %d\n  end: %d\n  size: %d\n\n",
+      da->capacity, 
+      da->origin, 
+      da->end,
+      DynamicArray_size(da));
+
+    free(s);
+
+}
+
+void DynamicArray_set(DynamicArray * da, int index, double value) {
+    assert(da->buffer != NULL);
+    assert ( index >= 0 );
+    while ( out_of_buffer(da, index_to_offset(da, index) ) ) {
+        extend_buffer(da);
+    }
+    da->buffer[index_to_offset(da, index)] = value;
+    if ( index >= DynamicArray_size(da) ) {
+        da->end = index_to_offset(da,index+1);
+    }
+
+}
+
+double DynamicArray_get(const DynamicArray * da, int index) {
+    assert(da->buffer != NULL);
+    assert ( index >= 0 );
+    if ( index >= DynamicArray_size(da) ) {
+        return 0;
+    } else {
+        return da->buffer[index_to_offset(da,index)];
+    }
+}
+
+void DynamicArray_push(DynamicArray * da, double value ) {
+    DynamicArray_set(da, DynamicArray_size(da), value );
+}
+
+void DynamicArray_push_front(DynamicArray * da, double value) {
+    assert(da->buffer != NULL);
+    while ( da->origin == 0 ) {
+        extend_buffer(da);
+    }
+    da->origin--;
+    DynamicArray_set(da,0,value);
+}
+
+double DynamicArray_pop(DynamicArray * da) {
+    assert(DynamicArray_size(da) > 0);
+    double value = DynamicArray_last(da);
+    DynamicArray_set(da, DynamicArray_size(da)-1, 0.0);
+    da->end--;
+    return value;
+}
+
+double DynamicArray_pop_front(DynamicArray * da) {
+    assert(DynamicArray_size(da) > 0);
+    double value = DynamicArray_first(da);
+    da->origin++;
+    return value;    
+}
+
+DynamicArray * DynamicArray_map(const DynamicArray * da, double (*f) (double)) {
+    DynamicArray * result = DynamicArray_copy(da);
+    for ( int i=0; i<DynamicArray_size(result); i++ ) {
+        DynamicArray_set(result, i, f(DynamicArray_get(result, i)));
+    }
+    return result;
+}
+
+DynamicArray * DynamicArray_subarray(DynamicArray * da, int a, int b) {
+
+  assert(da->buffer != NULL);
+
+  if ( b < a ) {
+      printf("DynamicArray_subarray called with invalid indices (b=%d<a%d)", b, a);
+      exit(1);
+  }
+
+  DynamicArray * result = DynamicArray_new();
+ 
+  for (int i=a; i<b; i++) {
+      DynamicArray_push(result,DynamicArray_get(da, i));
+  }
+
+  return result;
+
+}
+
+double DynamicArray_sum ( const DynamicArray * da ){ //check divide by zero
+   int sz;
+   double sum = 0;
+   assert(da->buffer != NULL);
+   //assert(DynamicArray_size(da) > 0);
+   sz = DynamicArray_size(da);
+   for(int i =0 ; i<sz; i++){
+       sum += DynamicArray_get(da, i);
+   } 
+   
+   return sum;
+}
+
+double DynamicArray_mean ( const DynamicArray * da ){ //check divide by zero
+   int sz;
+   double avg = 0;
+   assert(da->buffer != NULL);
+   assert(DynamicArray_size(da) > 0);
+   sz = DynamicArray_size(da);
+   avg = DynamicArray_sum (da);
+   avg /=sz;
+   return avg;
+}
+
+double DynamicArray_min ( const DynamicArray * da ){
+    double min;
+
+    assert(da->buffer != NULL);
+    assert(DynamicArray_size(da) > 0);
+
+    DynamicArray * sort_da =  DynamicArray_sort(da);
+    min =  DynamicArray_get(sort_da, 0);
+    DynamicArray_destroy(sort_da);  
+
+    return min;
+}
+double DynamicArray_max ( const DynamicArray * da ){
+    
+    double max;
+
+    assert(da->buffer != NULL);
+    assert(DynamicArray_size(da) > 0);
+
+    DynamicArray * sort_da =  DynamicArray_sort(da);
+    max =  DynamicArray_get(sort_da, DynamicArray_size(da)-1);
+    DynamicArray_destroy(sort_da);  
+
+    return max;
+}
+double DynamicArray_median ( const DynamicArray * da ){
+
+    double median = 0;
+
+    assert(da->buffer != NULL);
+    assert(DynamicArray_size(da) > 0);
+
+    DynamicArray * sort_da =  DynamicArray_sort(da);
+    DynamicArray_print_debug_info(sort_da);
+    
+    if(DynamicArray_size(sort_da)%2 == 0){
+        median = DynamicArray_get(sort_da, (int)DynamicArray_size(da)/2)+DynamicArray_get(sort_da, (int)((DynamicArray_size(da)/2)-1));
+        median /=2;
+    }
+    else{
+        median = DynamicArray_get(sort_da, (int)(DynamicArray_size(da)/2));
+    }
+
+    
+
+    DynamicArray_destroy(sort_da);  
+
+    return median;
+}
+/*not sure about first and last , if they shud return or change len as well*/
+double DynamicArray_last (const DynamicArray * da ){
+    assert(da->buffer != NULL);
+    assert(DynamicArray_size(da) > 0);
+    double last;
+    last =DynamicArray_get(da, DynamicArray_size(da)-1);
+    return last;
+}
+
+double DynamicArray_first (const DynamicArray * da ){
+    assert(da->buffer != NULL);
+    assert(DynamicArray_size(da) > 0);
+    double first;
+    
+    first = DynamicArray_get(da, 0);
+    return first;
+}
+
+DynamicArray * DynamicArray_copy ( const DynamicArray * da ){
+    assert(da->buffer != NULL);
+    assert(DynamicArray_size(da) > 0);
+
+    DynamicArray * copy_da = DynamicArray_new();
+    int sz = DynamicArray_size(da);
+    
+    for(int i = 0; i<sz; i++){
+        DynamicArray_set(copy_da, i, DynamicArray_get(da,i) );
+    }
+    return copy_da;
+}
+
+DynamicArray * DynamicArray_range ( double a, double b, double step){
+    
+    //assert(step > 0 ? a < b :a > b);  //Dont know how to set assertion if a>b with step also increasing
+    DynamicArray * da = DynamicArray_new();
+    int flag = 0;
+    if (step > 0){
+        if(a< b){
+            flag = 1;
+        }
+    }
+    else{
+        if(a > b){
+            flag =1 ;
+        }
+    }
+    if(flag){
+        int n = (b - a)/step;
+        n++;
+        for(int i = 0 ; i < n; i++  ){
+            DynamicArray_set(da,i,a+i*step);
+        }
+    }
+    
+
+    return da;
+}
+DynamicArray * DynamicArray_concat ( const DynamicArray * a, const DynamicArray * b ){
+    int sz1 = DynamicArray_size(a);
+    int sz2 = DynamicArray_size(b);
+    DynamicArray * da = DynamicArray_new();
+    for(int i = 0; i<sz1 ;i++){
+        DynamicArray_set(da, i, DynamicArray_get(a,i) );
+    }
+    for(int i = 0; i<sz2 ;i++){
+        DynamicArray_set(da, i+sz1, DynamicArray_get(b,i) );
+    }
+    return da;
+}
+
+DynamicArray * DynamicArray_take(const DynamicArray *da,const int n){
+    assert(da->buffer != NULL);
+    DynamicArray * take_da = DynamicArray_new(); 
+    int i = 0;
+    if(n > 0){
+        if(n < DynamicArray_size(da)){
+            for(int i = 0 ; i <n; i++){ //n +ve, take from start and append with zeros if surpasses
+                DynamicArray_set(take_da, i, DynamicArray_get(da,i) );
+            }
+        }
+        else{
+            for(int i = 0 ; i <DynamicArray_size(da); i++){ //n +ve, take from start and append with zeros if surpasses
+                DynamicArray_set(take_da, i, DynamicArray_get(da,i) );
+            }
+            for(int i = DynamicArray_size(da); i<n;i++){
+                DynamicArray_set(take_da, i, 0);
+            }
+        }
+    }
+    else{
+        //n is -ve take from back and append with zeros if surpassed
+        
+        if(abs(n) < DynamicArray_size(da)){
+            for(int i = DynamicArray_size(da) + n  ; i < DynamicArray_size(da); i++){ //n +ve, take from start and append with zeros if surpasses
+                DynamicArray_set(take_da, i- DynamicArray_size(da) - n , DynamicArray_get(da,i) );
+                //printf("%d",i);
+            }
+            
+        }
+        else{
+            for(int i = 0 ; i < DynamicArray_size(da) ; i++){ //n +ve, take from start and append with zeros if surpasses
+                DynamicArray_set(take_da, i, DynamicArray_get(da,i) );
+            }
+            
+            
+            for(int i = DynamicArray_size(da);i < abs(n);i++){
+                DynamicArray_set(take_da, i, 0);
+            }
+        }
+    }
+    return take_da;
+}
+
+int DynamicArray_is_valid( DynamicArray * da){
+    assert(da->buffer != NULL);
+    
+
+    assert (ptrtoDA->buffer != NULL);
+    int i = 0,flag = 0;
+    while(i < ArbitraryArray_size(ptrtoDA)){
+        DynamicArray** ptr_da = (DynamicArray**)ArbitraryArray_get_ptr(ptrtoDA,i);
+        
+        printf("Address of ptrtoDA : %d *ptrtoDA : %d ptr_da : %p *ptr_da %p; da %p &da: %d *da : %p\n",ptrtoDA,*ptrtoDA, ptr_da, *ptr_da, da,&da,*da);
+        printf("Condition %d",((*ptr_da) == (da)));
+
+        if(*ptr_da == da){
+            printf("/n works\n");
+            flag = 1;
+            break;
+        }
+        
+        i++;
+    }
+    if(flag){
+        return 1;
+    }
+    else{
+        printf("\n last else \n");
+        return 0;
+    }
+}
+
+/*! Returns the number of arrays that have been constructed to far.
+ */
+int DynamicArray_num_arrays(){
+    assert(ptrtoDA->buffer != NULL);
+    int i = 0,count = 0;
+    while(i < ArbitraryArray_size(ptrtoDA)){
+        if(ArbitraryArray_get_ptr(ptrtoDA,i) != 0){
+            count ++;
+        }
+        i++;
+    }
+    return count;
+}
+
+/*! Destroys all arrays that have been constructed so far.
+ */
+int DynamicArray_destroy_all(){
+    assert(ptrtoDA->buffer != NULL);
+    int i = 0;
+    while(i < ArbitraryArray_size(ptrtoDA)){
+        if(ArbitraryArray_get_ptr(ptrtoDA,i) != 0){
+            DynamicArray_destroy((DynamicArray *)ArbitraryArray_get_ptr(ptrtoDA,i));
+        }
+        i++;
+    }
+}
+
---
+++ hw_3/arrays/unit_tests_example_hw3.c
@@ -0,0 +1,333 @@
+#include <math.h>
+#include <float.h> /* defines DBL_EPSILON */
+#include "dynamic_array.h"
+#include "arbitrary_array.h"
+#include "gtest/gtest.h"
+
+#define X 1.2345
+
+typedef struct {
+    double x, y, z;
+} Point;
+
+typedef struct {
+            double a;
+            int b;
+            double c;
+} simply;
+
+char * point_to_string ( void * p ) {
+    Point * q = (Point *) p;
+    char * str = (char *) calloc(40, sizeof(char));
+    snprintf(str,40,"{%.1lf,%.1lf,%.1lf}", q->x, q->y, q->z);
+    return str;
+}
+
+char * simply_to_string ( void * s ) {
+    simply * q = (simply *) s;
+    char * str = (char *) calloc(40, sizeof(char));
+    snprintf(str,40,"{%.1lf,%d,%.1lf}", q->a, q->b, q->c);
+    return str;
+}
+
+char * double_to_string(void * d){
+    double * dp =  (double *) d;
+    char * str = (char *) calloc(40, sizeof(char));
+    snprintf(str,40,"{%.1lf}",*dp);
+    return str;
+}
+
+namespace {
+/*
+    TEST(DynamicArray, Min_Max){
+        DynamicArray * da = DynamicArray_new();
+        DynamicArray_push(da, 35.0);
+        DynamicArray_push(da, -2.0);
+        DynamicArray_push(da, 18.0);
+        ASSERT_DOUBLE_EQ(DynamicArray_max(da), 35.0);
+        ASSERT_DOUBLE_EQ(DynamicArray_min(da), -2.0);
+        DynamicArray_pop_front(da);
+        ASSERT_DOUBLE_EQ(DynamicArray_max(da), 18.0);
+        DynamicArray_pop(da);
+        ASSERT_DOUBLE_EQ(DynamicArray_max(da), -2.0);
+        ASSERT_DOUBLE_EQ(DynamicArray_min(da), -2.0);
+        DynamicArray_pop(da);
+        ASSERT_DEATH(DynamicArray_max(da), ".*Assertion.*");
+        DynamicArray_destroy(da);
+        free(da);
+    }
+
+    TEST(DynamicArray, Median){
+        DynamicArray * da = DynamicArray_new();
+        DynamicArray_push(da, 2.0);
+        DynamicArray_push(da, 10.0);
+        DynamicArray_push(da, 21.0);
+        DynamicArray_push(da, 3.0);
+        DynamicArray_push(da, 11.0);
+        ASSERT_DOUBLE_EQ(DynamicArray_median(da), 10.0);
+        DynamicArray_push(da, 4.0);
+        ASSERT_DOUBLE_EQ(DynamicArray_median(da), 7.0);
+        DynamicArray_destroy(da);
+        free(da);
+    }
+
+    TEST(DynamicArray, Sum_Mean){
+        DynamicArray * da = DynamicArray_new();
+        ASSERT_DOUBLE_EQ(DynamicArray_sum(da), 0);
+        ASSERT_DEATH(DynamicArray_mean(da), ".*Assertion.*");
+        double sum = -5.2+6.8+9.9+(-4.35)+0.77;
+        double mean = sum/5.0;
+        DynamicArray_push(da, -5.2);
+        DynamicArray_push(da, 6.8);
+        DynamicArray_push(da, 9.9);
+        DynamicArray_push(da, -4.35);
+        DynamicArray_push(da, 0.77);        
+        ASSERT_DOUBLE_EQ(DynamicArray_sum(da), sum);
+        ASSERT_DOUBLE_EQ(DynamicArray_mean(da), mean);
+        DynamicArray_destroy(da);
+        free(da);
+    }
+    
+    TEST(DynamicArray, First_Last){
+        DynamicArray * da = DynamicArray_new();
+        DynamicArray_push(da, 23.0);
+        DynamicArray_push(da, 45.0);
+        ASSERT_DOUBLE_EQ(DynamicArray_first(da), 23.0);
+        ASSERT_DOUBLE_EQ(DynamicArray_last(da), 45.0);
+        DynamicArray_pop(da);
+        ASSERT_DOUBLE_EQ(DynamicArray_first(da), 23.0);
+        ASSERT_DOUBLE_EQ(DynamicArray_last(da), 23.0);
+        DynamicArray_pop(da);
+        ASSERT_DEATH(DynamicArray_first(da), ".*Assertion.*");
+        ASSERT_DEATH(DynamicArray_last(da), ".*Assertion.*");
+        DynamicArray_destroy(da);
+        free(da);
+    }
+
+    TEST(DynamicArray, Copy){
+        DynamicArray * da = DynamicArray_new();
+        for (int i = 0; i < 5; i++){
+            DynamicArray_push(da, i);
+        }
+        DynamicArray *ptr = DynamicArray_copy(da);
+        for (int i = 0; i < 5; i++){
+            ASSERT_EQ(DynamicArray_get(ptr, i), i);
+        }
+        DynamicArray_set(ptr, 2, 101);
+        ASSERT_EQ(DynamicArray_get(da, 2), 2);
+        ASSERT_EQ(DynamicArray_get(ptr, 2), 101);
+        DynamicArray_destroy(da);
+        DynamicArray_destroy(ptr);
+        free(da);
+        free(ptr);
+    }
+
+    TEST(DynamicArray, Range){
+        DynamicArray * a = DynamicArray_range(1.0, 2.0, 0.3);
+        for(int i=0; i<DynamicArray_size(a); i++){
+            ASSERT_LT(
+                abs(DynamicArray_get(a,i) - (1.0 + i*0.3)),
+                DBL_EPSILON
+            );
+        }
+        DynamicArray * b = DynamicArray_range(7.0, 1.0, -2);
+        for(int i=0; i<DynamicArray_size(b); i++){
+            ASSERT_LT(
+                abs(DynamicArray_get(b,i) - (7.0 - i*2)),
+                DBL_EPSILON
+            );
+        }
+        DynamicArray * c = DynamicArray_range(6, 4, 3);
+        ASSERT_EQ(DynamicArray_size(c), 0);
+        ASSERT_DEATH(DynamicArray_first(c), ".*Assertion.*");
+        DynamicArray_destroy(a);
+        DynamicArray_destroy(b);
+        DynamicArray_destroy(c);
+        free(a);
+        free(b);
+        free(c);
+    }
+
+    TEST(DynamicArray, Concat)
+    {
+        DynamicArray *a = DynamicArray_new();
+        DynamicArray *b = DynamicArray_range(1, 2, 0.1);
+        DynamicArray *c = DynamicArray_concat(a, b);
+        for (int i = 0; i < DynamicArray_size(b); i++)
+        {
+            ASSERT_DOUBLE_EQ(DynamicArray_get(c, DynamicArray_size(a) + i), 1 + 0.1 * i);
+        }
+        DynamicArray_destroy(a);        
+        DynamicArray_destroy(b);
+        DynamicArray_destroy(c);
+        free(a);
+        free(b);
+        free(c);
+    }
+    TEST(DynamicArray, Take){
+        DynamicArray *a = DynamicArray_range(-2, 3, 1);
+        DynamicArray *b = DynamicArray_take(a, 2);
+        for (int i = 0; i < DynamicArray_size(b); i++)
+        {
+            ASSERT_DOUBLE_EQ(DynamicArray_get(b, i), -2 + i);
+        }
+        DynamicArray *c = DynamicArray_take(a, -2); 
+        for (int i = 0; i < DynamicArray_size(c); i++)
+        {
+            ASSERT_DOUBLE_EQ(DynamicArray_get(c, i), i+2);
+        }
+        DynamicArray *d = DynamicArray_take(a, 10); 
+        for (int i = 6; i < DynamicArray_size(d); i++)
+        {
+            ASSERT_DOUBLE_EQ(DynamicArray_get(c, i), 0);
+        }
+        DynamicArray_destroy(a);        
+        DynamicArray_destroy(b);
+        DynamicArray_destroy(c);
+        DynamicArray_destroy(d);
+        free(a);
+        free(b);
+        free(c);
+        free(d);
+    }
+
+
+    TEST(ArbitraryArray, String){
+        
+        
+        Point p = {1, 2, 3}, q = {4, 5, 6};
+        ArbitraryArray *a = ArbitraryArray_new(sizeof(Point));
+        
+        ArbitraryArray_set_from_ptr(a, 0, &p);
+
+        ArbitraryArray_set_from_ptr(a, 1, &q);
+        
+        char *s = ArbitraryArray_to_string(a, point_to_string);
+        ASSERT_STREQ(s, "{1.0,2.0,3.0},{4.0,5.0,6.0}");
+        
+        ArbitraryArray_destroy(a);
+        free(a);
+
+        
+       Point p2 = {1.1, 2.1, 3.1}, q2 = {4.1, 5.1, 6.1};
+        ArbitraryArray *a2 = ArbitraryArray_new(sizeof(Point));
+        
+        ArbitraryArray_set_from_ptr(a2, 0, &p2);
+
+        ArbitraryArray_set_from_ptr(a2, 1, &q2);
+        
+        char *s2 = ArbitraryArray_to_string(a2, point_to_string);
+        ASSERT_STREQ(s2, "{1.1,2.1,3.1},{4.1,5.1,6.1}");
+        
+        ArbitraryArray_destroy(a2);
+        free(a2);
+
+        
+        
+
+
+
+       simply p3 = {1.1, 2, 3.1}, q3 = {4.1, 5, 6.1};
+        ArbitraryArray *a3 = ArbitraryArray_new(sizeof(simply));
+        
+        ArbitraryArray_set_from_ptr(a3, 0, &p3);
+
+        ArbitraryArray_set_from_ptr(a3, 1, &q3);
+        
+        char *s3 = ArbitraryArray_to_string(a2, simply_to_string);
+        ASSERT_STREQ(s3, "{1.1,2,3.1},{4.1,5,6.1}");
+        
+        ArbitraryArray_destroy(a3);
+        free(a3);
+    
+        double p4 = 1.2, q4 = 4.2;
+        ArbitraryArray *a4 = ArbitraryArray_new(sizeof(double));
+        
+        ArbitraryArray_set_from_ptr(a4, 0, &p4);
+
+        ArbitraryArray_set_from_ptr(a4, 1, &q4);
+        
+        char *s4 = ArbitraryArray_to_string(a4, double_to_string);
+        ASSERT_STREQ(s4, "{1.2},{4.2}");
+        
+        ArbitraryArray_destroy(a4);
+        free(a4);
+        
+        
+    }
+
+    TEST(ArbitraryArray,OfPointers) {
+
+    // Create the array that will hold the pointers
+    ArbitraryArray * ptrs = ArbitraryArray_new(sizeof(DynamicArray *));
+
+    // Create a couple of dynamic arrays
+    DynamicArray * a = DynamicArray_new(),
+                            * b = DynamicArray_new();
+
+    // Add the dynamic arrays to the pointer array
+    ArbitraryArray_set_from_ptr(ptrs, 0, &a);
+    ArbitraryArray_set_from_ptr(ptrs, 1, &b);
+
+    // Get them back. Note that because the array contains pointers,
+    // and _get_ptr returns a pointer to a pointer, we need to cast
+    // its result as a pointer to a DynamicArray pointer.
+    DynamicArray ** ptr_a = (DynamicArray **) ArbitraryArray_get_ptr(ptrs,0);
+    DynamicArray ** ptr_b = (DynamicArray **) ArbitraryArray_get_ptr(ptrs,1);
+
+    // Check that the pointers are equal
+    ASSERT_EQ(*ptr_a, a);
+    ASSERT_EQ(*ptr_b, b);
+    ASSERT_NE(*ptr_a, b);
+    ASSERT_NE(*ptr_b, a);
+    ArbitraryArray_destroy(ptrs);
+    free(ptrs);
+    DynamicArray_destroy(a);
+    free(a);
+    }*/
+
+    
+    TEST(DynamicArray, All){
+        //DynamicArray_destroy_all();
+
+        DynamicArray *a = DynamicArray_range(0, 1, 0.1);
+        ASSERT_EQ(DynamicArray_num_arrays(), 1);
+        ASSERT_EQ(DynamicArray_is_valid(a), 1);
+
+
+        /*DynamicArray *a = DynamicArray_range(0, 1, 0.1);
+        ASSERT_EQ(DynamicArray_is_valid(a), 1);
+        ASSERT_EQ(DynamicArray_num_arrays(), 1);
+        
+        DynamicArray *b = DynamicArray_range(1.1, 2, 0.1);
+        ASSERT_EQ(DynamicArray_num_arrays(), 2);
+        
+        DynamicArray *c = DynamicArray_concat(a, b);
+        ASSERT_EQ(DynamicArray_num_arrays(), 3);
+        
+        DynamicArray *d = DynamicArray_take(a, -3);
+        ASSERT_EQ(DynamicArray_num_arrays(), 4);
+        
+        DynamicArray *e = DynamicArray_copy(b);
+        ASSERT_EQ(DynamicArray_num_arrays(), 5);
+        
+        DynamicArray *f = DynamicArray_copy(b);
+        ASSERT_EQ(DynamicArray_num_arrays(), 6);
+        /*
+        DynamicArray_destroy(f);
+        free(f);
+         ASSERT_EQ(DynamicArray_num_arrays(), 5);
+        /*
+        DynamicArray_destroy_all();
+        ASSERT_EQ(DynamicArray_is_valid(a), 0);
+        ASSERT_EQ(DynamicArray_num_arrays(), 0);
+        free(a);
+        
+        free(b);
+        
+        free(c);
+        free(d);
+        free(e);*/
+        //free(f);
+    } 
+}
